b36bd861478d3b138fde11192c4936dee75af0e3
Nasca Octavian PAUL <zynaddsubfx@yahoo.com>
2015-07-15 21:04:25 +0300

    comments

--- run_demo.sh
+++ run_demo.sh
@@ -6,3 +6,5 @@ oggdec in_*.ogg
 ./2xphases.py -s 10 -a 0.7 in_arpeggio.wav in_librivox.wav -o out_convolution_arpeggio_librivox.wav
 ./2xphases.py -s 10 in_librivox.wav in_reverb.wav -o out_convolution_librivox_reverb.wav
 
+#todo add 2xautoconvolution.py examples
+
